66a1d2068d353e755fd8464f71847c90c95cddac
Madison <midunitz@ucdavis.edu>
2016-06-06 18:35:27 -0700

    Server setup

--- SQL_server/index.js
+++ SQL_server/index.js
@@ -4,7 +4,24 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
-
-const router = express.Router();
 const Promise = require('bluebird');
 
+const config = require('../config')
+
+
+const router = express.Router();
+
+
+
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json({type: '*/*'}));
+
+router
+
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+app.listen(port)
