329f77fb785cced0f79b2921df795e608fc9494c
Madison <midunitz@ucdavis.edu>
2016-09-05 10:44:35 -0700

    add daily TODO actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,10 @@ import {
   AUTH_ERROR, 
   UNAUTH_USER, 
   FETCH_MESSAGE,
-  DAILY_REVIEW 
+  DAILY_REVIEW,
+  ADD_TODO,
+  SET_VISIBILITY_FILTER,
+  TOGGLE_TODO 
 } from './types';
 
 
@@ -104,6 +107,31 @@ export function dailyReview(){
     payload: request
   }
 }
+
+//daily todos
+let nextTodoId = 0;
+export const addTodo = (text) => {
+  return {
+    type: ADD_TODO,
+    id: nextTodoId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  }
+}
+
+export const toggleTodo = (id) => {
+  return {
+    type: TOGGLE_TODO,
+    id
+  }
+}
+
 
 // export const FIRST_ACTION = 'NOTES';
 
--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,7 @@ export const FETCH_MESSAGE = 'fetch_message';
 export const CREATE_GOAL = 'CREATE_GOAL';
 export const FETCH_GOALS = 'FETCH_GOALS';
 export const DAILY_REVIEW ='DAILY_REVIEW';
+// daily TODOs
+export const ADD_TODO = 'ADD_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
